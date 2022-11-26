d745eeed9ee9dfa8d10f31f0793073ace0b2411b
Nathan Boukobza <boukobza.nathan@gmail.com>
2022-11-26 10:33:50 +0100

    add admin handler

--- personne_app/admin.py
+++ personne_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from personne_app.models import Personne
 
-# Register your models here.
+
+@admin.register(Personne)
+class PersonneAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'birthday')
+    search_fields = ('last_name', 'first_name', 'birthday')
--- personne_app/models.py
+++ personne_app/models.py
@@ -5,3 +5,6 @@ class Personne(models.Model):
     last_name = models.CharField(verbose_name='nom', max_length=200)
     first_name = models.CharField(verbose_name='prénom', max_length=200)
     birthday = models.DateField()
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
